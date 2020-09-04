0917ccd76ee673ea32eed90af7a9a2b0ef42ff36
caiolucasb <caio.lucas75@gmail.com>
2020-09-04 10:21:27 -0300

    unsing useRouterMatch to take informations of repositories(params)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
 import React, {useState, FormEvent, useEffect} from 'react';
+import { Link } from "react-router-dom";
+
 
 import api from '../../services/api'
 
@@ -70,14 +72,14 @@ const Dashboard: React.FC = () => {
             {inputError && <Error>{inputError}</Error>}
             <Repositories>
                 {repositories.map( repository => (
-                    <a key={repository.full_name} href= "">
+                    <Link key={repository.full_name} to= {`/Repository/${repository.full_name}`}>
                         <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
                     <div>
                 <strong>{repository.full_name}</strong>
                         <p>{repository.description}</p>
                     </div>
                     <FiChevronRight size={20}/>
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
